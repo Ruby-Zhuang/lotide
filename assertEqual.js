960b18fd2f52dd5267ddf8e12edff52b2f7f00ed
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-03-29 14:59:55 -0400

    Create assertEqual function for lotide project

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual(1, 3);
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
