2d53844eb3d57df272a2994bb7f6f932508beaa9
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-03-29 15:38:59 -0400

    Add head function to return first element of an array

---
+++ head.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  } else {
+    return;
+  }
+};
+
+//TESTS
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([2]), 2);
+
