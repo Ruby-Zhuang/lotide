8dcbdc017933ad7cedaa9dbf6482a7a8ace459b6
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-05 10:37:26 -0400

    Refactor middle function to export it and separate it's test code

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,3 @@
-// const assertEqual = require("./assertEqual");
 // EQARRAYS FUNCTION
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
--- middle.js
+++ middle.js
@@ -1,8 +1,4 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = require("./eqArrays");
-const assertArraysEqual = require("./assertArraysEqual");
-
-// ACTUAL FUNCTION
+// MIDDLE FUNCTION
 const middle = function(array) {
   let middleElements = [];
   const arrayLength = array.length;
@@ -22,11 +18,5 @@ const isEven = function(num) {
   return (num % 2 === 0);
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, "3", "4", 5, 6]), ["3", "4"]);
+// MODULE EXPORTS
+module.exports = middle;
